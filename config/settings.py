80f8ee9dea2e7e604fcd3de62c7fbf90b7b32f5b
david <d.conchacid@gmail.com>
2023-07-23 01:34:56 -0400

    vercel

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "config/static")]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "stacticfiles", "static")
